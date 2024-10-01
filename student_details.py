deb764eb71a1c2323726e93b283d3a0d50c39dd9
ganga8089 <gangav2028@cs.sjcetpalai.ac.in>
2024-10-01 14:53:52 +0530

    my first commit

---
+++ student_details.py
@@ -0,0 +1,9 @@
+
+name=input("enter the student name:")
+roll_no=int(input("enter roll number"))
+cgpa=float(input("enter your cgpa"))
+percentage_marks=cgpa * 10
+roll_no=roll_no+1
+print("name-",name)
+print("roll_no.-",roll_no)
+print("mark percentage",percentage_marks,"%")
