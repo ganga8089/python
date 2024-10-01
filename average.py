ac2a66d24b14f2f553239583d73361247368f12f
ganga <gangav2028@cs.sjcetpalai.ac.in>
2024-10-01 15:21:07 +0530

    Add files via upload

---
+++ average.py
@@ -0,0 +1,11 @@
+'''
+Author : Ganga
+Date :01-10-2024
+python program to find average of three numbers
+'''
+
+num_1=int(input("enter 1st number:"))
+num_2=int(input("enter 2nd number:"))
+num_3=int(input("enter 3rd number:"))
+avg=(num_1+num_2+num_3)/3
+print("average=",avg)
