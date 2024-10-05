82bb013a6de686d9b0fd0c06a95639f9efb8d3b8
ganga <gangav2028@cs.sjcetpalai.ac.in>
2024-10-05 15:37:38 +0530

    Add files via upload

---
+++ stringdemo.py
@@ -0,0 +1,9 @@
+'''
+name:ganga
+version:1.1
+date:05/10/2024
+Write a Python program that stores a string in a variable. Extract a specific part of the string (substring) and then concatenate it with another string. Finally, display the new string.
+'''
+str_1="Hello world!"
+str_2=str_1[6:11]
+print(str_2+" Everyone")
