2628561077e6a5dda430ec2dbeeb594cfbe9702f
ganga <gangav2028@cs.sjcetpalai.ac.in>
2024-10-05 15:36:41 +0530

    Add files via upload

---
+++ st_details.py
@@ -0,0 +1,9 @@
+'''
+Name:ganga
+date:05/10/2024
+version:1.1
+write a python program that asks the user to input their name and age.After receiving the input,display a message that includes the user's name and age.
+'''
+name=input("Enter your name:")
+age=int(input("Enter your age:"))
+print("Hello,",name,"! You are",age,"years old")
