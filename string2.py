2812f3b5442bbd4f57ff8f9a3463e1b91290dac3
ganga <gangav2028@cs.sjcetpalai.ac.in>
2024-10-05 15:37:04 +0530

    Add files via upload

---
+++ string2.py
@@ -0,0 +1,8 @@
+'''
+name:ganga
+version:1.1
+date:05/10/2024
+Write a Python program to print the following string in a specific format
+'''
+str="Twinkle, twinkle, little star,\n \t How I wonder what you are!\n \t\t Up above the world so high,\n \t\t Like a diamond in the sky.\n  Twinkle, twinkle, little star,\n \t How I wonder what you are"
+print(str)
