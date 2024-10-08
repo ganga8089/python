5ce61653e495d8d745673f8f4adbe73fc0077107
ganga <gangav2028@cs.sjcetpalai.ac.in>
2024-10-08 14:53:32 +0530

    Add files via upload

---
+++ arithmetic_2.py
@@ -0,0 +1,21 @@
+'''
+name:ganga
+version:1.1
+date:08/10/2024
+to perform arithmetic operations
+'''
+num1=int(input("enter first number:"))
+num2=int(input("enter second number"))
+num3=int(input("enter third number"))
+sum=num1+num2
+print("The sum of num1 and num2 is:",sum)
+difference=num2-num1
+print("The difference when num2 is subtracted from num1 is:",difference)
+product=num1*num2
+print( "The product of num1 and num2 is:",product)
+quotient=num1/num2
+print("The quotient when num1 is divided by num2 is:",quotient)
+remainder=num1%num2
+print("The remainder when num1 is divided by num2 is:",remainder)
+result = (num1 + num2) * num3 / 2
+print("The result of (num1 + num2) * num3 / 2 is:",result)
