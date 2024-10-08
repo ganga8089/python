b9db444d7edba870432aefc643c86ace4de77f26
ganga <gangav2028@cs.sjcetpalai.ac.in>
2024-10-08 15:09:25 +0530

    Add files via upload

--- string_concatenation.py
+++ string_concatenation.py
@@ -1,12 +1,12 @@
 '''
 name:ganga
-version:1.1
+version:1.2
 date:08/10/2024
 '''
-str1="Ganga"
-str2="V"
+str1=input("Enter first name")
+str2=input("Enter second name")
 Name=str1+" "+str2
 print(Name)
 length=len(Name)
-extract=Name[length-1]
+extract=Name[:length-1]
 print(extract)
