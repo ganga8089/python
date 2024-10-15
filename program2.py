379e649d0fc99f628e22816e2da8f54c079f2324
ganga <gangav2028@cs.sjcetpalai.ac.in>
2024-10-15 14:23:16 +0530

    Add files via upload

---
+++ program2.py
@@ -0,0 +1,12 @@
+'''
+name=ganga
+date:15/10/2024
+version:1.1
+python program to check whether a person is eligible for driving liscence or not.
+      The minimum age requirement is 16 years.
+'''
+age=int(input("Enter your age:"))
+if age>=16:
+    print("you are eligible.")
+else:
+    print("you are not eligibe")
