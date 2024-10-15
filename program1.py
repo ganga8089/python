b15d11bfdba45924f1e29b53fed7d30b4bd9b762
ganga <gangav2028@cs.sjcetpalai.ac.in>
2024-10-15 14:17:04 +0530

    Add files via upload

---
+++ program1.py
@@ -0,0 +1,11 @@
+'''
+name=ganga
+date:15/10/2024
+version:1.1
+python program to check whether a given number is greater than 100.
+'''
+number=int(input("Enter a number:"))
+if number>100:
+    print("The given number is greater than 100")
+else:
+    print("The given number is less than 100")
