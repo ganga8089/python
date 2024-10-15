81b66e757f6d0f07b7e117ce4e3bb9fbb5e0877b
ganga <gangav2028@cs.sjcetpalai.ac.in>
2024-10-15 14:27:53 +0530

    Add files via upload

---
+++ program3.py
@@ -0,0 +1,11 @@
+'''
+name=ganga
+date:15/10/2024
+version:1.1
+python program to check whether a number is divisible by 5 or not
+'''
+number=int(input("Enter a number:"))
+if number%5==0:
+    print(number,"is divisible by 5")
+else:
+    print(number,"is not divisible by 5")
