c12ae09cb795bb8b17001ac9f003806e7d5e3350
ganga <gangav2028@cs.sjcetpalai.ac.in>
2024-10-15 15:44:03 +0530

    Add files via upload

---
+++ program12.py
@@ -0,0 +1,15 @@
+'''
+name=ganga
+date:15/10/2024
+version:1.1
+python program to print colour based on code value
+'''
+grade=input("Enter grade:")
+if grade=='R':
+    print("Message=Red")
+elif grade=='G':
+    print("Message=green")
+elif grade=='B':
+    print("Message=blue")
+else:
+    print("Wrong code")
