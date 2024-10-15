02ee49b938d15b75e7796a03019343f7b5a421c1
ganga <gangav2028@cs.sjcetpalai.ac.in>
2024-10-15 14:44:20 +0530

    Add files via upload

---
+++ program5.py
@@ -0,0 +1,12 @@
+'''
+name=ganga
+date:15/10/2024
+version:1.1
+python program to print numbers from 1 to 10
+'''
+limit=int(input("Enter the+ limit"))
+number=1
+while(number<=limit):
+    print(number)
+    number=number+1
+
