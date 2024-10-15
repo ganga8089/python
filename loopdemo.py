1a1623acac73751460aa26ea81110ef3f1243faa
ganga <gangav2028@cs.sjcetpalai.ac.in>
2024-10-15 14:03:35 +0530

    Add files via upload

---
+++ loopdemo.py
@@ -0,0 +1,11 @@
+'''
+name=ganga
+date:15/10/2024
+version:1.1
+python program to check whether a given number is positive or not
+'''
+number=int(input("Enter a number:"))
+if number>0:
+    print("The given number:",number,"is positive")
+else:
+    print("The given number",number,"is negative")
