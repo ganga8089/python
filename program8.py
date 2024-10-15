f80b90e53803a44bc2ef07ed7a028da627199300
ganga <gangav2028@cs.sjcetpalai.ac.in>
2024-10-15 15:03:40 +0530

    Add files via upload

---
+++ program8.py
@@ -0,0 +1,11 @@
+'''
+name=ganga
+date:15/10/2024
+version:1.1
+python program to find simple interest
+'''
+p=int(input("Enter principle amount:"))
+n=int(input("Enter number of years:"))
+r=int(input("Enter rate of interest:"))
+SI=p*n*r
+print("Simple interest=",SI)
