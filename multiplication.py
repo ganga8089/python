7e27a45b4f7f8b56d4473405399275d0f1921798
ganga <gangav2028@cs.sjcetpalai.ac.in>
2024-10-29 15:17:23 +0530

    Add files via upload

---
+++ multiplication.py
@@ -0,0 +1,9 @@
+'''
+Name:Ganga
+date:29-10-2024
+python program to find multiplication of a given number
+'''
+number=int(input("enter number"))
+for i in range(1,13):
+    product=number*i
+    print(f" {number} * {i} = {product}")
