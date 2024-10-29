f6fb504534c458520a13a69399be87292480f2f5
ganga <gangav2028@cs.sjcetpalai.ac.in>
2024-10-29 15:20:53 +0530

    Add files via upload

---
+++ multi_2.py
@@ -0,0 +1,9 @@
+'''
+Name:Ganga
+date:29-10-2024
+'''
+number=int(input("enter number"))
+limit =int(input("enter the limit"))
+for i in range(1,limit+1):
+    product=number*i
+    print(f" {number} * {i} = {product}")
